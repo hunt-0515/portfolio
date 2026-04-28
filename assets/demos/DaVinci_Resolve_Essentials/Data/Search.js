e6e039977f983a9c6286b2b309d914c015ee520f
Nathan <nathan923@gmail.com>
2026-04-27 22:32:51 -0400

    Added DaVinci Resolve Site demo

---
+++ assets/demos/DaVinci_Resolve_Essentials/Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],m:[0],u:['../Content/DaVinci Help/0 Cover.htm'],s:['+'],p:[['+',2],['the',4]]});
---
+++ assets/demos/DaVinci_Resolve_Essentials/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=639120407541994838'
+});
